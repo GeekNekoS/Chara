402a83e3bef9327a12c41fe184bab1aa00c719c5
Дмитрий <dimakol2005@mail.ru>
2024-11-02 18:56:12 +0500

    task7 del wrong code parts

--- module/create_model.py
+++ module/create_model.py
@@ -4,9 +4,7 @@ from tensorflow.keras import layers
 def create_model(input_shape: tuple, num_classes: int):
 
     inputs = keras.Input(shape=input_shape)
-    dense = layers.Dense(64, activation="relu")
-    x = dense(inputs)
-    x = layers.Conv2D(64, 3, 1, activation='relu', padding='same')(x)
+    x = layers.Conv2D(64, 3, 1, activation='relu', padding='same')(inputs)
     x = layers.BatchNormalization()(x)
     x = layers.MaxPooling2D(2)(x)
     x = layers.Conv2D(128, 3, 1, activation='relu', padding='same')(x)
