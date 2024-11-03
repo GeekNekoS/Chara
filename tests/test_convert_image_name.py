4744cf368fd181cc882e62a2fb9179c5edf94e8d
Jane <milkey_way74@mail.ru>
2024-11-03 15:38:18 +0500

    tests_convertation

---
+++ tests/test_convert_image_name.py
@@ -0,0 +1,43 @@
+import os
+from PIL import Image
+from pathlib import Path
+import pytest
+from module.convert_image_name import convert_image_name_to_jpg
+
+
+@pytest.fixture
+
+def create_test_folder(tmpdir):
+    """Создание тестовой папки с изображениями для проверки."""
+    test_folder = tmpdir.mkdir("test_images")
+  
+    subfolder1 = test_folder.mkdir("subfolder1")
+    subfolder2 = test_folder.mkdir("subfolder2")
+    #Создание тестовых изображений
+    with open(os.path.join(str(subfolder1), "image1.png"), "wb") as f:
+        f.write(b"Test image data")
+    with open(os.path.join(str(subfolder2), "image2.bmp"), "wb") as f:
+        f.write(b"Test image data")
+  
+    return str(test_folder)
+
+def test_convert_image_name_to_jpg_with_subfolders(create_test_folder):
+    """Проверка конвертации изображений в подпапках."""
+    convert_image_name_to_jpg(create_test_folder)
+    # Проверка создания подпапок для конвертированных изображений
+    assert os.path.exists(os.path.join(create_test_folder, "converted_images", "subfolder1"))
+    assert os.path.exists(os.path.join(create_test_folder, "converted_images", "subfolder2"))
+    
+    # Проверка формата jpg
+    assert os.path.exists(os.path.join(create_test_folder, "converted_images", "subfolder1", "image1.jpg"))
+    assert os.path.exists(os.path.join(create_test_folder, "converted_images", "subfolder2", "image2.jpg"))
+
+def test_convert_image_name_to_jpg_with_invalid_file(create_test_folder):
+  """Проверка обработки неверных файлов."""
+  # Создание файла, не являющегося изображением
+  with open(os.path.join(create_test_folder, "subfolder1", "invalid.txt"), "w") as f:
+    f.write("Test text")
+  
+  convert_image_name_to_jpg(create_test_folder)
+  # Проверка результата конвертации файла, не являющегося изображением
+  assert not os.path.exists(os.path.join(create_test_folder, "converted_images", "subfolder1", "invalid.txt"))
