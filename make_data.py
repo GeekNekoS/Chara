fa33669a4d8cf5dd69dc1650cc83ddef91bc8f34
GeekNekoS <116801392+geeknekos@users.noreply.github.com>
2024-11-05 21:13:40 +0500

    dataset made with file make_data.py

--- make_data.py
+++ make_data.py
@@ -8,3 +8,4 @@ from module.convert_image_name import convert_image_name
 if __name__ == '__main__':
     datadir = '/mnt/c/Users/unive/Downloads/MBTI MMPI-20241030T170531Z-001/MBTI MMPI'
     edit_image_name(datadir)
+    convert_image_name(datadir)
