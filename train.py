d38e24bb1107c653f8be4c8bcc476d3345d4e3d4
GeekNekoS <anastasiyanesterenko@icloud.com>
2024-11-07 18:18:09 +0500

    Documentation for functions: create_model and load_train_test_val - added

--- train.py
+++ train.py
@@ -1,7 +1,22 @@
 """
 Модуль для тренировки нейронной сети/модели.
 """
-from module.
+from module.project_logging import setup_logger
+from module.load_train_test_val import load_train_test_val
+from module.create_model import create_model
+
+logger = setup_logger("Learning model started")
 
 if __name__ == '__main__':
-    pass
+    directory = '/mnt/d/project_practicum/dataset'
+    batch_size = 32
+    image_size = (150, 150)
+    shuffle = True
+
+    train_dataset, test_dataset, val_dataset = load_train_test_val(
+        directory=directory,
+        batch_size=batch_size,
+        image_size=image_size,
+        shuffle=shuffle,
+    )
+    model = create_model()
