36740f53a8c811e10a6e8870d8ab56dc01c63f1b
Vlad Kalashnikov <kalashnikovv537@gmail.com>
2025-01-11 13:44:27 +0500

    train file refactoring

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import numpy as np
 from module.project_logging import setup_logger
 from module.train_model import train_model
 from module.evaluate_model import evaluate_model
-from module.load_train_test_val import load_train_test_val
+from module.load_train_test import load_train_test
 from module.predict_class import predict_class
 
 
@@ -21,7 +21,7 @@ def main(directory,
          learning_rate,
          evaluate
          ):
-    train_dataset, test_dataset = load_train_test_val(directory, batch_size, image_size)
+    train_dataset, test_dataset = load_train_test(directory, batch_size, image_size)
     if not evaluate:
         train_model(
             train_dataset=train_dataset,
@@ -36,13 +36,13 @@ def main(directory,
     predicted_classes, labels = predict_class(test_dataset, batch_size, image_size)
     evaluate_model(predicted_classes, labels)
 
+
 if __name__ == '__main__':
-    directory = '/mnt/d/project_practicum/dataset128_fixed/dataset'
-    # directory = '/mnt/d/project_practicum/mnist_images'
+    directory = 'dataset_fixed'
     batch_size = 16
     image_size = (64, 64)   # (28, 28) (64, 64) (256, 256)
-    num_classes = 128 # 58 10 для mnist
-    epochs = 200
+    num_classes = 128  # 58 10 для mnist
+    epochs = 50
     learning_rate = 1e-03
     evaluate = False  # False - обучить и оценить, True - только оценить
     main(directory, batch_size, image_size, num_classes, epochs, learning_rate, evaluate=evaluate)
