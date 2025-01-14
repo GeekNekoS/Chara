a8b2c737072889ae1da4d949b49ff1024e169358
Vlad Kalashnikov <kalashnikovv537@gmail.com>
2025-01-14 21:41:12 +0500

    tg bot refactoring

---
+++ tg_bot/results_handler.py
@@ -0,0 +1,24 @@
+import json
+import numpy as np
+
+
+def handle_results(results, file_path="metrics.json"):
+    """Загружает вопросы из JSON-файла."""
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            metrics = json.load(file)
+            answer = ''
+            for result in results:
+                current_metric = metrics.get(str(result[0] + 1))
+                answer += current_metric + f' Вероятность: {result[1]:.4f}' + '\n'
+            return answer
+    except FileNotFoundError:
+        print("Файл не найден!")
+        return []
+    except json.JSONDecodeError:
+        print("Ошибка декодирования JSON!")
+        return []
+
+
+if __name__ == "__main__":
+    print(handle_results([[0, 10], [2, 9]]))
